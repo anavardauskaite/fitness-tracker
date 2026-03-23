570c3d0b65d15d492287e6c7a9262349390144b8
Augustina Navardauskaitė-Grigutė <a.navardauskaite@gmail.com>
2026-03-23 19:11:09 +0200

    pridejom XOXO

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,12 @@ export default function App() {
   return (
     <>
       <header className="sticky top-0 z-10 bg-light dark:bg-dark p-4 border-b-2 border-slate-200 dark:border-slate-800">
-        Convex + React + Convex Auth
+        ConvexXOXO + React + Convex Auth
         <SignOutButton />
       </header>
       <main className="p-8 flex flex-col gap-16">
         <h1 className="text-4xl font-bold text-center">
-          Convex + React + Convex Auth
+          ConvexXOXO + React + Convex Auth
         </h1>
         <Authenticated>
           <Content />
